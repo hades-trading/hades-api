9c70dff25267513763a29c087ce0b88e6d1aafdf
Kuo Guo <gary.guo.china@gmail.com>
2023-07-26 21:35:35 +0800

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,1 +1,2 @@
 GUNICORN_CMD_ARGS="--timeout 600 --access-logfile /tmp/app_access.log --error-logfile /tmp/app_error.log -c /opt/startup/gunicorn.conf.py --chdir=$APP_PATH --workers 4 --worker-class uvicorn.workers.UvicornWorker" gunicorn main:app
+
